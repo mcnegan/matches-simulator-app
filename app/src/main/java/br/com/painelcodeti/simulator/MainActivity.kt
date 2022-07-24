ea019224af704fb4df72f8aa7fb1face067d6e25
mcnegan <mcpantoj@gmail.com>
2022-07-24 09:25:05 -0300

    first commit Icon

--- app/src/main/java/br/com/painelcodeti/simulator/MainActivity.kt
+++ app/src/main/java/br/com/painelcodeti/simulator/MainActivity.kt
@@ -1,11 +1,17 @@
 package br.com.painelcodeti.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+
     }
 }
