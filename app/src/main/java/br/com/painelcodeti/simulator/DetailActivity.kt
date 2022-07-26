f07246a28d2a24b4785c89f0b5d156455e9db95b
mcnegan <mcpantoj@gmail.com>
2022-07-25 22:00:18 -0300

    Create Layout List detail

---
+++ app/src/main/java/br/com/painelcodeti/simulator/DetailActivity.kt
@@ -0,0 +1,25 @@
+package br.com.painelcodeti.simulator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.painelcodeti.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+
+    }
+
+}
